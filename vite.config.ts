2a60cfa8a766f41c92829e42849e6cdcb0d474b7
xxpsynagure <pragnya.nagure2002@gmail.com>
2024-02-02 02:23:15 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/portfolio-pragnya/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000, 
